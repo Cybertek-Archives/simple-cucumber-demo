210aafa151345b58d630b258b1eabb3f8ca279e4
marufjont <marufjon@cybertekschool.com>
2019-02-18 17:06:38 -0500

    added screenshot option

--- src/test/java/cbt/StepDefs.java
+++ src/test/java/cbt/StepDefs.java
@@ -1,44 +1,44 @@
 package cbt;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.fail;
-
 public class StepDefs {
-	WebDriver driver;
+    WebDriver driver;
 
-	@Given("^I am on the home page$")
-	public void i_am_on_the_home_page() throws Throwable {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("http://etsy.com");
+    @Given("^I am on the home page$")
+    public void i_am_on_the_home_page() throws Throwable {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://etsy.com");
 
-	}
+    }
 
-	@When("^I search for \"([^\"]*)\"$")
-	public void i_search_for(String search) throws Throwable {
-		driver.findElement(By.id("search-query")).sendKeys(search + Keys.ENTER);
-	}
+    @When("^I search for \"([^\"]*)\"$")
+    public void i_search_for(String search) throws Throwable {
+        driver.findElement(By.id("search-query")).sendKeys(search + Keys.ENTER);
+    }
 
-	@Then("^I should be see the results$")
-	public void i_should_be_see_the_results() throws Throwable {
-		Assert.assertTrue(driver.getCurrentUrl().contains("search"));
-		driver.quit();
-	}
-	
-	@Then("^I should be see the list$")
-	public void i_should_be_see_the_resultss() throws Throwable {
-		Assert.assertTrue(driver.getCurrentUrl().contains("search"));
-		driver.quit();
-		fail();
-	}
+    @Then("^I should see the results$")
+    public void i_should_see_the_results() throws Throwable {
+        Assert.assertTrue(driver.getCurrentUrl().contains("search"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        driver.quit();
+    }
+
 
 }
--- src/test/resources/cbt/simple_demo.feature
+++ src/test/resources/cbt/simple_demo.feature
@@ -4,10 +4,10 @@ Feature: The application should be running
   Scenario: simple search
     Given I am on the home page
     When I search for "wooden spoon"
-    Then I should be see the results
+    Then I should see the results
 
   @smoke
   Scenario: another search
     Given I am on the home page
     When I search for "useless box"
-    Then I should be see the results
+    Then I should see the results
